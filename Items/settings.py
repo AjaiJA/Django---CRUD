cb1fa6b2301c8158d45606bab1414839c55293c2
AjaiJA <ajaija2000@gmail.com>
2021-06-30 11:11:17 +0530

    Updated wsgi file

--- Items/settings.py
+++ Items/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 
+STATIC_ROOT='staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
