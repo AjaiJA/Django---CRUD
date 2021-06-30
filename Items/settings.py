1252b7f3e6df9a4ed542a2f83d7165503da4ba1e
AjaiJA <ajaija2000@gmail.com>
2021-06-30 11:15:01 +0530

    Updated settings file

--- Items/settings.py
+++ Items/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bba@rg_2symsuzcf5f*8=p=91^20imv_u=lvb#82(=y-c_o=u_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
